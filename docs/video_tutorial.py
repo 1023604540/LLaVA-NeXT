b970e909af526c5e93e9b09dbc933aee53a6c8d7
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-16 18:00:30 +0100

    add key memory selection

--- docs/video_tutorial.py
+++ docs/video_tutorial.py
@@ -158,7 +158,7 @@ image_tensors.append(frames)
 # Prepare conversation input
 conv_template = "qwen_1_5"
 
-question = f"{DEFAULT_IMAGE_TOKEN}\n Can you see a woman sitting in front of the desk? "
+question = f"{DEFAULT_IMAGE_TOKEN}\n What is happening in the video?"
 conv = copy.deepcopy(conv_templates[conv_template])
 conv.append_message(conv.roles[0], question)
 conv.append_message(conv.roles[1], None)
