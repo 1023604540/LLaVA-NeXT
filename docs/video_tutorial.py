845200974a03b5658fa4269b89c1caad23b49f39
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-17 21:34:30 +0100

    add key memory selection

--- docs/video_tutorial.py
+++ docs/video_tutorial.py
@@ -158,7 +158,7 @@ image_tensors.append(frames)
 # Prepare conversation input
 conv_template = "qwen_1_5"
 
-question = f"{DEFAULT_IMAGE_TOKEN}\n Describe the man."
+question = f"{DEFAULT_IMAGE_TOKEN}\n Describe the car."
 conv = copy.deepcopy(conv_templates[conv_template])
 conv.append_message(conv.roles[0], question)
 conv.append_message(conv.roles[1], None)
