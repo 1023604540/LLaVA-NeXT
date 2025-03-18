1f2861c0faf183cf26229dad2983ace2f45d7b7c
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-18 20:31:54 +0100

    testing vision encoder

--- scripts/train/finetune_ov.sh
+++ scripts/train/finetune_ov.sh
@@ -27,7 +27,7 @@ echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
 # Stage 2
 PROMPT_VERSION="qwen_1_5"
-RUN_NAME="llava-onevision-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-memory_adapter_no_compression"
+RUN_NAME="llava-onevision-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-global_memory_adapter"
 PREV_STAGE_CHECKPOINT="/anvme/workspace/b232dd16-LLaVA-OV/llava-onevision-qwen2-7b-ov" # replace it with your last checkpoint training from single image collection
 echo "PREV_STAGE_CHECKPOINT: ${PREV_STAGE_CHECKPOINT}"
 echo "MID_RUN_NAME: ${RUN_NAME}"
@@ -45,7 +45,7 @@ ACCELERATE_CPU_AFFINITY=0 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --deepspeed scripts/zero2.json \
     --model_name_or_path $PREV_STAGE_CHECKPOINT \
     --version $PROMPT_VERSION \
-    --data_path /home/hpc/b232dd/b232dd16/LLaVA-OV/scripts/train/sharegpt_train.yaml \
+    --data_path /home/hpc/b232dd/b232dd16/LLaVA-OV/scripts/train/3_video_datasets.yaml \
     --image_folder /anvme/workspace/b232dd21-zyr/llava-data \
     --video_folder /anvme/workspace/b232dd16-LLaVA-OV/llava-video \
     --mm_tunable_parts="attention_model,mm_mlp_adapter" \
@@ -85,7 +85,7 @@ ACCELERATE_CPU_AFFINITY=0 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --torch_compile_backend "inductor" \
     --dataloader_drop_last True \
     --force_sample True \
-    --frames_upbound 128   # 32 initially
+    --frames_upbound 150   # 32 initially
 exit 0;
 
 # You can delete the sdpa attn_implementation if you want to use flash attn
