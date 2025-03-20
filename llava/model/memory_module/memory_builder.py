b2137a3b192eb1c90fd7d44692c69991740e49c5
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-20 14:52:34 +0100

    use only retrieved memory

--- llava/model/memory_module/memory_builder.py
+++ llava/model/memory_module/memory_builder.py
@@ -152,6 +152,7 @@ class MultimodalOpsMixin:
                 key_length = 32
                 if key_centroids.shape[0] > key_length:
                     key_centroids = key_centroids[:key_length]
+                print(f"long_memory.shape = {long_memory.shape}, key_centroids.shape = {key_centroids.shape}")
                 dists = ((long_memory.unsqueeze(1) - key_centroids.unsqueeze(0)) ** 2).sum(dim=3).sum(dim=2).sqrt()
                 min_indices = torch.argmin(dists, dim=0)
                 min_indices_sorted, _ = torch.sort(min_indices)
