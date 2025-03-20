c58de2c5b8abcdb0839fcf83703169a9926e9e0f
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-20 12:08:10 +0100

    use only retrieved memory

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -401,6 +401,7 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
                 #     raise ValueError("NaNs detected in attention_model output!")
                 # rank0_print(
                 #     f"[attention_model] output requires_grad={cat_segment_memory.requires_grad}, grad_fn={cat_segment_memory.grad_fn}")
+                print(f"Encoded features shape : {encoded_features.shape}")
                 memory_feature = self.compress_temporal_features(encoded_features, video_idx_in_batch, all_video=True)
                 images_list[idx] = memory_feature
 
