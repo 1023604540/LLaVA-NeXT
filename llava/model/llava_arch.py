c831ba9d56912c1fd7f9e7f413499489aebb7769
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-21 13:42:40 +0100

    fix dtype bug

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -377,6 +377,7 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
 
                 segment_memory = []
                 encoded_features = self.encode_images(image)
+                print(f"Encoded features shape : {encoded_features.shape}, {encoded_features[0].shape}")
                 encoded_features = encoded_features.requires_grad_()
                 # print(
                 #     f"[DEBUG] Vision output requires_grad={encoded_features.requires_grad}, grad_fn={encoded_features.grad_fn}")
