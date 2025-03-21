08e8b440526da54e75365a52b314362461d1409a
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-21 01:28:08 +0100

    try to add siglip text encoder

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -257,8 +257,8 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
         image_feature = image_feature.view(num_frames, -1, num_dim)
         return image_feature
 
-    def encode_images(self, images):
-        image_features = self.get_model().get_vision_tower()(images)
+    def encode_images(self, images, text):
+        image_features = self.get_model().get_vision_tower()(images, text)
         # image_features = self.get_model().vision_resampler(image_features, images=images)
         # image_features = self.get_model().mm_projector(image_features)
         return image_features
