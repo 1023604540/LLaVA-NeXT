460a52024e616fdff3d693fe83659dd384e72dc5
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-24 13:03:14 +0100

    path change

--- scripts/train/finetune_ov.sh
+++ scripts/train/finetune_ov.sh
@@ -27,12 +27,13 @@ echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
 # Stage 2
 PROMPT_VERSION="qwen_1_5"
-RUN_NAME="llava-onevision-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-correct_memory_adapter"
+# RUN_NAME="llava-onevision-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-correct_memory_adapter"
+RUN_NAME="llava-onevision-correct_memory_adapter"
 PREV_STAGE_CHECKPOINT="lmms-lab/llava-onevision-qwen2-7b-ov" # replace it with your last checkpoint training from single image collection
 echo "PREV_STAGE_CHECKPOINT: ${PREV_STAGE_CHECKPOINT}"
 echo "MID_RUN_NAME: ${RUN_NAME}"
 
-NUM_GPUS=1
+NUM_GPUS=8
 NNODES=$SLURM_NNODES
 RANK=$SLURM_PROCID
 ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n1)  # Master node
