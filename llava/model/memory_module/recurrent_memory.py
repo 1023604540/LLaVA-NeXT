1ef6256126c265c43a7cdefeafb359fd1ac31b6b
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-25 01:05:44 +0100

    debug

--- llava/model/memory_module/recurrent_memory.py
+++ llava/model/memory_module/recurrent_memory.py
@@ -286,7 +286,7 @@ class TransformerProjector(nn.Module):
         # (1) Decide what your "current memory" is:
         if len(self.memory_cache) == 0:
             # First call: use initial_memory
-            current_memory = self.initial_memory.to(device=device, dtype=dtype)
+            current_memory = self.initial_memory.to(device=device, dtype=dtype).to("cuda")
             if torch.isnan(current_memory).any():
                 raise ValueError("NaNs detected in current_memory!")
         else:
