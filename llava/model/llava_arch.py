635388d4a7144b64e620e6f6fa5ec36226681bd0
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-25 02:00:26 +0100

    debug

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -99,10 +99,10 @@ class LlavaMetaModel:
             if "unpad" in getattr(config, "mm_patch_merge_type", ""):
                 self.image_newline = nn.Parameter(torch.empty(config.hidden_size, dtype=self.dtype))
 
-        # self.mm_input_dim = getattr(config, "ntm_hidden_size", 1152)
-        # compress_Turing_hidden_dim = getattr(config, "compress_Turing_hidden_dim", 32)
+        self.mm_input_dim = getattr(config, "ntm_hidden_size", 1152)
+        compress_Turing_hidden_dim = getattr(config, "compress_Turing_hidden_dim", 32)
         # Now initiate the memory_builder
-        self.attention_model = NeuralTuringMachine().to(self.device)
+        self.attention_model = NeuralTuringMachine(self.mm_input_dim, compress_Turing_hidden_dim).to(self.device)
         # self.memory_mlp = nn.Sequential(
         #     nn.Linear(1152, 1152),
         #     nn.GELU(),
