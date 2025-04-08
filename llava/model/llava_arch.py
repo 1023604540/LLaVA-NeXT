98bef9be511f8a36cfa01fc5a3c0aa2530d36bc0
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-08 22:57:47 +0200

    skip memory

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -383,22 +383,15 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
                 encoded_features = self.encode_images(image)
                 # encoded_features = encoded_features.requires_grad_()
                 print(f"encoded_features shape : {encoded_features.shape}")
-                memory = self.compress_temporal_features(encoded_features)
+                # memory = self.compress_temporal_features(encoded_features)
                 #print(f"Segment memory : {[x.shape for x in segment_memory if x is not None]}")
-                # torch.cuda.synchronize()
-                # print("After attention_model forward pass")
 
-                # sep_token_id = 151647
-                # sep_token_tensor = torch.tensor([sep_token_id], device=image.device)
-                # sep_embedding = self.get_model().embed_tokens(sep_token_tensor)
-
-
-                if torch.isnan(memory).any():
-                    raise ValueError("NaNs detected in attention_model output!")
+                #if torch.isnan(memory).any():
+                    #raise ValueError("NaNs detected in attention_model output!")
                 # rank0_print(f"cat_segment_memory shape : {cat_segment_memory.shape}")
                 # rank0_print(
                 #     f"[attention_model] output requires_grad={cat_segment_memory.requires_grad}, grad_fn={cat_segment_memory.grad_fn}")
-                images_list[idx] = memory
+                #images_list[idx] = memory
                 images_list[idx] = encoded_features
 
             # Now process all non-video images together.
