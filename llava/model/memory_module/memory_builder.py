bab6734ab91b6fc873529ee2004ede79e5945afa
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-08 21:58:22 +0200

    skip memory

--- llava/model/memory_module/memory_builder.py
+++ llava/model/memory_module/memory_builder.py
@@ -172,9 +172,9 @@ class MultimodalOpsMixin:
         if Turing_memory_compressed.shape[0] < video_Turing_memory_length:
             Turing_memory_compressed = Turing_memory[:0]
         memory_feature = torch.cat([
-            Turing_memory_compressed.view(-1, 729, 1152),   #（9，9*9，1152）（1，729，1152）
+            #Turing_memory_compressed.view(-1, 729, 1152),   #（9，9*9，1152）（1，729，1152）
             #long_memory_compressed.view(-1, 729, 1152),
-            #cur_memory.view(-1, 729, 1152),
+            cur_memory.view(-1, 729, 1152),
         ], dim=0).to(dtype=image_features.dtype)
 
         mem_shape = memory_feature.shape  # e.g. (frames, 729, 1152)
