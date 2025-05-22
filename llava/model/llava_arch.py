c71624f979065a35b63e6f5127be9058ee31dd45
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-23 00:32:04 +0200

    use original frame input

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -486,13 +486,13 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
                     # rank_print(f"updated_image_segment shape : {updated_image_segment.shape}")
                     # rank_print(f"recurrent_memory shape : {recurrent_memory.shape}")
                 # Branch dropout the updated image segment
-                dropout_rate = getattr(self.config, "recurrent_dropout_rate", 0.2)
-                if self.training:
-                    number = torch.rand(1, device=updated_image_segment.device).item()
-                    # print(f"random number is {number}")
-                    if number < dropout_rate:
-                        updated_image_segment = torch.zeros(updated_image_segment.shape).to(device=self.device,dtype=self.dtype)
-                        rank_print(f"updated_image_segment dropout")
+                # dropout_rate = getattr(self.config, "recurrent_dropout_rate", 0.2)
+                # if self.training:
+                #     number = torch.rand(1, device=updated_image_segment.device).item()
+                #     # print(f"random number is {number}")
+                #     if number < dropout_rate:
+                #         updated_image_segment = torch.zeros(updated_image_segment.shape).to(device=self.device,dtype=self.dtype)
+                #         rank_print(f"updated_image_segment dropout")
 
             original_frames_idx = torch.linspace(0, image.shape[0] - 1,
                                                         steps=32)  # Sample 8 frames as initial memory
