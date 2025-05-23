48fe6a801c080dca4b3e18323a8ba568eb80867e
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-23 14:08:29 +0200

    add shape print

--- llava/model/language_model/modeling_qwen2.py
+++ llava/model/language_model/modeling_qwen2.py
@@ -258,6 +258,7 @@ class Qwen2Attention(nn.Module):
 
         query_states = self.q_proj(hidden_states)
         key_states = self.k_proj(hidden_states)
+        print("key_states.shape original", key_states.shape)
         value_states = self.v_proj(hidden_states)
 
         query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
