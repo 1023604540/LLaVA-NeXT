0ff603efa039de96fcd5063a7853701cbf3cced5
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-27 00:30:05 +0200

    change init of self.memory_gate

--- llava/model/language_model/modeling_qwen2.py
+++ llava/model/language_model/modeling_qwen2.py
@@ -237,7 +237,10 @@ class Qwen2Attention(nn.Module):
             base=self.rope_theta,
         )
         # Gating parameter for memory prompts, initialized to zero
-        self.memory_gate = nn.Parameter(torch.zeros(1, self.num_heads, 1, 1))
+        # self.memory_gate = nn.Parameter(torch.zeros(1, self.num_heads, 1, 1))
+        self.memory_gate = nn.Parameter(torch.empty(1, self.num_heads, 1, 1))
+
+
     def forward(
         self,
         hidden_states: torch.Tensor,
