f77e3ef2088fe283321818f6a3f302d6247ea45e
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-01 18:11:37 +0200

    original pipelime

--- scripts/train/finetune_ov_4.sh
+++ scripts/train/finetune_ov_4.sh
@@ -89,7 +89,7 @@ srun --mpi=pmix --export=ALL,ACCELERATE_CPU_AFFINITY=0 \
     --tf32 True \
     --model_max_length 32768 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 1 \
     --dataloader_pin_memory True \
     --lazy_preprocess True \
     --report_to wandb \
