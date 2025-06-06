94443a8d159eba49b1560179a1a929a049c5900e
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-06 16:31:00 +0200

    use a random tensor to replace recurrent memory

--- scripts/train/finetune_ov_4.sh
+++ scripts/train/finetune_ov_4.sh
@@ -28,7 +28,7 @@ echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
 # Stage 2
 PROMPT_VERSION="qwen_1_5"
-RUN_NAME="llava-onevision-0.5b-qwen2_KIT_position_8tokens_adapter_with_rotary_embedding"
+RUN_NAME="llava-onevision-0.5b-qwen2_KIT_position_8tokens_adapter_random"
 # PREV_STAGE_CHECKPOINT="/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/checkpoints/llava-onevision-qwen2-0.5b-ov" # replace it with your last checkpoint training from single image collection
 PREV_STAGE_CHECKPOINT="lmms-lab/llava-onevision-qwen2-0.5b-ov"
 
